3dff382774fccf0f6518adebfb0aed66b4bd2b94
Kevin Mortimer <kmortime@gmail.com>
2023-10-18 13:49:07 -0400

    mission 5 changes

--- mission_five.py
+++ mission_five.py
@@ -15,12 +15,22 @@ from robot_18300 import robot_18300
 def mission_five(r):
     print("Running Mission 5")
     #madeleine going to the museum
-    r.robot.straight(500)
+    r.robot.straight(564)
     r.robot.turn(-45)
     r.robot.straight(250)
     r.robot.turn(-45)
-    r.robot.straight(399)
+    r.robot.straight(460)
     r.robot.turn(60)
     r.robot.straight(275)
+    r.robot.turn(75)
+    r.robot.straight(101)
+    speed = 200
+    turn = 60
+    distance = 136
+    r.robot.reset()
+
+    # While the robot.distance() is less than or equal to the variable 'distance' stay in the while loop
+    while (r.robot.distance() <= distance):
+        r.robot.drive(speed,turn)
     
     
