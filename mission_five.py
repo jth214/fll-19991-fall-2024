f84845ad1eaf6b6531aa38a6422da435a5656a83
Kevin Mortimer <kmortime@gmail.com>
2023-12-01 17:45:34 -0500

    lydias changes

--- mission_five.py
+++ mission_five.py
@@ -21,31 +21,31 @@ def mission_five(r):
     #Robot is 
     #facing the set change mission.
     r.robot.turn(-45)
-    r.robot.straight(-351)
+    r.robot.straight(-321)
     #turn torward wall
     #r.robot.turn(90)
-    r.robot.drive(-100,35)
+    r.robot.drive(-200,75)
     wait(3011)
     #The robot is now at the wall
     r.robot.stop()
     r.robot.straight(37)
-    r.robot.turn(15)
+    r.robot.turn(5)
     r.robot.straight(291)
     #Backing up
-    r.left_attachment_motor.run_time(-300,6000, then=Stop.HOLD, wait=False)
+    r.left_attachment_motor.run_time(-300,10000, then=Stop.HOLD, wait=False)
     wait(5000)
     r.robot.straight(50)
     r.robot.turn(37)
     r.robot.stop()
     #Robot is Facing Augmented reality.
-    r.robot.drive(-75,0)
-    wait(2000)
-    r.robot.stop()
+    # r.robot.drive(-75,0)
+    # wait(2000)
+    #r.robot.stop()
     #At Augmented reality
-    r.gyro_tank_turn(70,50) 
-    r.robot.stop()
-    r.robot.straight(-123)
-    r.robot.turn(-30)
+    # r.gyro_tank_turn(70,50) 
+    #r.robot.stop()
+    #r.robot.straight(-123)
+    #r.robot.turn(-30)
     #Augmented reality is now complreted.
     
 
