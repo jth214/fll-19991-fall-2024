94cc12e19ffcbf2fddc4a20ee27de3e065ff513b
kmortime <96500206+kmortime@users.noreply.github.com>
2024-08-27 09:10:59 -0400

    Update mission_six.py

--- mission_six.py
+++ mission_six.py
@@ -14,39 +14,5 @@ from robot_18300 import robot_18300
 
 def mission_six(r):
     print("Running Mission 6")
-
-     # Create and define distance, speed, and turn variables
-    # to control how fast the robot moves, how sharp it turns, and how far it will go.
-    # Reset the robot distance to zero
-    speed = 200
-    turn = 45
-    distance = 112
-    r.robot.reset()
-
-    # While the robot.distance() is less than or equal to the variable 'distance' stay in the while loop
-    while (r.robot.distance() <= distance):
-        r.robot.drive(speed,turn)
-
-     # Create and define distance, speed, and turn variables
-    # to control how fast the robot moves, how sharp it turns, and how far it will go.
-    # Reset the robot distance to zero
-    speed = 200
-    turn = -60
-    distance = 136
-    r.robot.reset()
-
-    # While the robot.distance() is less than or equal to the variable 'distance' stay in the while loop
-    while (r.robot.distance() <= distance):
-        r.robot.drive(speed,turn)
-    
-    print("The robot has moved " + str(r.robot.distance()) + " mm")
-
-    # r.robot.drive() goes on forever, even after the while loop ends.
-    # You need to stop the robot after exiting the while loop
-    r.robot.stop()
-    
-    print("The robot has moved " + str(r.robot.distance()) + " mm")
-
-    # r.robot.drive() goes on forever, even after the while loop ends.
-    # You need to stop the robot after exiting the while loop
-    r.robot.stop()
+    # Mission Name
+    # Authors
