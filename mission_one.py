6aca6611a60143352bf2493548b5898cb44dea7e
jth214 <jth214@gmail.com>
2024-09-08 18:51:36 -0400

    Test mission 1

--- mission_one.py
+++ mission_one.py
@@ -16,3 +16,10 @@ def mission_one(r):
     print("Running Mission 1")
     # Mission Name
     # Authors
+    r.ev3.screen.clear()
+    print("Running Mission 1")
+    r.ev3.screen.draw_text(30, 60, "Mission 1")
+    wait(100)
+    # Mission Name
+    # Authors
+    r.gyro_drive_straight_distance(speed=500,distance=2000)
