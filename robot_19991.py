52a4eca20b0e40ebe762aea98e34539222ee46a3
jth214 <jth214@gmail.com>
2024-09-22 14:36:50 -0400

    New PD value

--- robot_19991.py
+++ robot_19991.py
@@ -27,7 +27,7 @@ MIN_TANK_TURN_SPEED = 30
 WHEEL_RADIUS = WHEEL_DIAMETER/2
 AXLE_TRACK_CM = AXLE_TRACK_MM/10
 GYRO_GAIN = 3
-GYRO_PD = 4
+GYRO_PD = 6 #4 is also a good value
 GYRO_MOUNT = "up" # or "down"
 
 ################################
